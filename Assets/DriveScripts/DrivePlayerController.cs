850b300f79b5d6f463f242c85719038fd3006760
KevinThe1st <klabel@calpoly.edu>
2020-03-02 21:23:43 -0800

    stage 3 changes

---
+++ Assets/DriveScripts/DrivePlayerController.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrivePlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float verticalInput;
+    public float maxSpeed = 40.0f;
+    private float turnSpeed = 55.0f;
+    public bool isTouchingObstacle;
+    private DriveGameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("DriveGameManager").GetComponent<DriveGameManager>();
+        gameManager.setSpeed(0f);
+        maxSpeed = 40.0f;
+        isTouchingObstacle = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        // Gas is on.  Move forwards
+        if (verticalInput > 0)
+        {
+            if (gameManager.getSpeed() < maxSpeed)
+            {
+                if (gameManager.getSpeed() < 0)
+                {
+                    gameManager.incrementSpeed(2);
+                }
+                gameManager.incrementSpeed(0.6f);
+            }
+            
+            else
+            {
+                gameManager.setSpeed(maxSpeed);
+            }
+        }
+        // Reverse is on.  Move backwards
+        else if (verticalInput < 0)
+        {
+            if (gameManager.getSpeed() > -maxSpeed)
+            {
+                if (gameManager.getSpeed() > 0)
+                {
+                    gameManager.decrementSpeed(2);
+                }
+                gameManager.decrementSpeed(0.6f);
+            }
+            if (gameManager.getSpeed() < -maxSpeed)
+            {
+                gameManager.setSpeed(-maxSpeed);
+            }
+        }
+        // Car is not getting gas.  Slow down
+        else if (verticalInput == 0)
+        {
+            // Car is moving forwards
+            if (gameManager.getSpeed() > 0)
+            {
+                gameManager.decrementSpeed(1.0f);
+                
+                if (gameManager.getSpeed() < 0)
+                {
+                    gameManager.setSpeed(0);
+                }
+            }
+            // Car is moving backwards
+            else if (gameManager.getSpeed() < 0)
+            {
+                gameManager.incrementSpeed(1.0f);
+
+                if (gameManager.getSpeed() > 0)
+                {
+                    gameManager.setSpeed(0);
+                }
+            }
+            
+        }
+
+        transform.Translate(Vector3.forward * gameManager.getSpeed() * Time.deltaTime);
+
+        if (gameManager.getSpeed() != 0)
+        {
+            transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime * horizontalInput);
+        }
+
+        if (isTouchingObstacle)
+        {
+            if (gameManager.getSpeed() > 0)
+            {
+                gameManager.decrementSpeed(1.5f);
+                if (gameManager.getSpeed() < 0)
+                {
+                    gameManager.setSpeed(0);
+                }
+            }
+            if (gameManager.getSpeed() < 0)
+            {
+                gameManager.incrementSpeed(1.5f);
+                if (gameManager.getSpeed() > 0)
+                {
+                    gameManager.setSpeed(0);
+                }
+            }
+        }
+        
+    }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            isTouchingObstacle = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            isTouchingObstacle = false;
+        }
+    }
+}
---
+++ Assets/DriveScripts/DriveZombieController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DriveZombieController : MonoBehaviour
+{
+    private Transform target;
+    private NavMeshAgent agent;
+
+    private GameObject player;
+    public bool isActive;
+    public float speed = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.Find("DriverPlayer");
+        target = player.transform;
+        isActive = false;
+        agent.speed = 4f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Vector3.Distance(player.transform.position, gameObject.transform.position) < 120f)
+        {
+            isActive = true;
+        }
+
+        if (Vector3.Distance(player.transform.position, gameObject.transform.position) > 120f)
+        {
+            isActive = false;
+        }
+
+        if (isActive)
+        {
+            //transform.LookAt(player.transform);
+            agent.SetDestination(target.position);
+        }
+    }
+}
