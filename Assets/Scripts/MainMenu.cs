7594c3714b5c7b6ccf7adc527f2eafb9fce8a0c9
unknown <stanedo@calpoly.edu>
2020-03-02 17:09:16 -0800

    stage 4 fixed and added UX for each scene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -18,4 +18,24 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
     }
+    public void Shop()
+    {
+        SceneManager.LoadScene(9);
+    }
+    public void SceneScreen()
+    {
+        SceneManager.LoadScene(10);
+    }
+    public void ScenePlay1()
+    {
+        SceneManager.LoadScene(7);
+    }
+    public void ScenePlay2()
+    {
+        SceneManager.LoadScene(6);
+    }
+    public void ScenePlay4()
+    {
+        SceneManager.LoadScene(11);
+    }
 }
