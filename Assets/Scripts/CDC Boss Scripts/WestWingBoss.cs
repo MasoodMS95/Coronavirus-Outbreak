5bcb683e2c96c76f675dc01de0bb2b588bde5be4
Masood Saaed <36909914+MasoodMS95@users.noreply.github.com>
2020-03-06 00:00:34 -0800

    Added first boss
    Still needs to be finished and debugged
    as it stand right now the CDC level won't compile, has a bug with the west wing boss script I believe.

---
+++ Assets/Scripts/CDC Boss Scripts/WestWingBoss.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WestWingBoss : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AttackDownwards();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void AttackDownwards()
+    {
+        while(transform.position.z < 19f)
+        {
+            transform.Translate(Vector3.forward * 10f * Time.deltaTime);
+        }
+        transform.Rotate(0, 90, 0);
+        while (transform.position.z > -11f)
+        {
+            transform.Translate(Vector3.forward * 10f * Time.deltaTime);
+        }
+        transform.Rotate(0, -90, 0);
+    }
+}
